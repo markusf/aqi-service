e45afdf06162ac1ce7847ce3f826b7f8070e373e
markusf <markus.feindler@gmx.de>
2016-04-25 21:34:40 +0800

    use user agent header to retrieve correct value

--- src/main/java/com/bluebird/aqi/service/AqiClient.java
+++ src/main/java/com/bluebird/aqi/service/AqiClient.java
@@ -1,11 +1,13 @@
 package com.bluebird.aqi.service;
 
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
 public interface AqiClient {
 	
 	@RequestLine("GET /city/{city}")
+	@Headers({"User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"})
 	String getByCity(@Param("city") String city);
 	
 }
